729247b4811b2f61cf20f59bd29348d391b5c19a
Justin Lui <justincylui@gmail.com>
2020-02-20 19:34:48 -0800

    Update index.js
    don't use double color stops

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getSolidGradientString = (colors = [], spacerSize = 0, rotation = 0) => {
+const getGradientString = (colors, spacerSize, rotation = 180) => {
   const numColors = colors.length;
   const numSpacers = numColors - 1;
   const totalSpacingBySpacer = numSpacers * spacerSize;
@@ -11,16 +11,32 @@ const getSolidGradientString = (colors = [], spacerSize = 0, rotation = 0) => {
   const strFragment = colors.map((color, index) => {
     const barStartColorStop = spacerSize * index + barSize * index;
     const barEndColorStop = spacerSize * index + barSize * (index + 1);
-    const result = [`${color} ${barStartColorStop}% ${barEndColorStop}%`];
+
+    // IE/Edge linear gradient support is finicky.
+    // cannot do double color stops.  For example:
+    // linear-gradient(45deg, black 25% 50%, white 50% 75%)
+
+    // So must be declare only one color and stop per entry only
+    // https://caniuse.com/#feat=mdn-css_types_image_gradient_linear-gradient_doubleposition
+    const result = [
+      `${color} ${barStartColorStop}%`,
+      `${color} ${barEndColorStop}%`
+    ];
 
     // add a spacer after every bar except after the last bar
     if (numColors !== index + 1) {
       const spacerEndColorStop =
         spacerSize * index + barSize * (index + 1) + spacerSize;
-      result.push(`transparent ${barEndColorStop}% ${spacerEndColorStop}%`);
+
+      result.push(
+        ...[
+          `transparent ${barEndColorStop}%`,
+          `transparent ${spacerEndColorStop}%`
+        ]
+      );
     }
 
     return result;
   });
-  return `linear-gradient(${rotation}deg, ${strFragment.join(",")})`;
+  return `linear-gradient(${rotation}deg, ${strFragment.join(',')})`;
 };
